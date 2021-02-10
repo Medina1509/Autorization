7b3b9ed5e595967ad08c180b77f4af1424779d6d
Medina1509 <dnkl09@list.ru>
2021-02-11 00:31:00 +0300

    Create Aop6KalmykApplicationTests.java

---
+++ aop6Kalmyk 2/src/test/java/M/Kalmyk/aop6Kalmyk/Aop6KalmykApplicationTests.java
@@ -0,0 +1,13 @@
+package M.Kalmyk.aop6Kalmyk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aop6KalmykApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
