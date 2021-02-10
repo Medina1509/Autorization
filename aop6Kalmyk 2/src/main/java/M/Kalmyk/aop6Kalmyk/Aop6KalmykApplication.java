bd373f1c66e20364d26b7f8ff8327a131ef51088
Medina1509 <dnkl09@list.ru>
2021-02-11 00:30:32 +0300

    Create Aop6KalmykApplication.java

---
+++ aop6Kalmyk 2/src/main/java/M/Kalmyk/aop6Kalmyk/Aop6KalmykApplication.java
@@ -0,0 +1,13 @@
+package M.Kalmyk.aop6Kalmyk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "M.Kalmyk.aop6Kalmyk")
+public class Aop6KalmykApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aop6KalmykApplication.class, args);
+	}
+
+}
